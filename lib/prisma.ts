9e5c122939b27f961d9f9a0429c1d3ef596767db
Milana <milka3341@gmail.com>
2025-02-17 19:00:54 +0200

    Refactor Prisma client initialization for Neon database adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,24 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import { Pool } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  if (process.env.DATABASE_ENV==="docker") {
-    return new PrismaClient();
-  } else {
-    const neon = new Pool({ connectionString: process.env.DATABASE_URL });
-    const adapter = new PrismaNeon(neon);
-    return new PrismaClient({ adapter });
-  }
-};
-
-declare global {
-  // eslint-disable-next-line no-var
-  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined
+const prismaClient = () => {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaNeon(neon);
+  return new PrismaClient({ adapter });
 }
 
-const prisma = global.prismaGlobal ?? prismaClientSingleton();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export default prisma;
+export const prisma =
+  globalForPrisma.prisma || prismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prismaGlobal = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
